108286df12c01c658416d48155d5ceb398363845
Kamil Czerniak <kamil_czerniak@outlook.com>
2018-10-18 17:43:19 +0200

    Implements steps 1-16 (without additional part).

---
+++ Recruiter/Controllers/JobOfferController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Recruiter.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Recruiter.Controllers
+{
+    public class JobOfferController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_jobOffers);
+        }
+        public IActionResult Details(int id)
+        {
+            return View(_jobOffers.Where(x=>x.Id==id).First());
+        }
+        private static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer{Id=1, JobTitle="Backend Developer"},
+            new JobOffer{Id=2, JobTitle="Frontend Developer"},
+            new JobOffer{Id=3, JobTitle="Manager"},
+            new JobOffer{Id=4, JobTitle="Teacher"},
+            new JobOffer{Id=5, JobTitle="Cook"},
+        };
+    }
+}
